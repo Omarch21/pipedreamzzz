796844af7161105e70557cdcb018608799977e1e
omarch21 <omarch641@gmail.com>
2022-04-08 14:31:49 -0500

    more things

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/*const express = require('express')
-app = express()
+const express = require('express')
+const app = express()
 
 var url = require('url');
 var dt = require('./date-time');
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/static'))
 
 // The app.get functions below are being processed in Node.js running on the server.
 // Implement a custom About page.
-app.get('/about', (request, response) => {
+app.get('/abouttttt', (request, response) => {
 	console.log('Calling "/about" on the Node.js server.')
 	response.type('text/plain')
 	response.send('About Node.js on Azure Template.')
@@ -31,7 +31,7 @@ app.get('/2plus2', (request, response) => {
 	response.type('text/plain')
 	response.send('4')
 })
-app.get('/',(req, res) =>{
+app.get('/h',(req, res) =>{
 	res.send('hey');
 })
 // Add x and y which are both passed in on the URL. 
@@ -121,35 +121,14 @@ app.get('/batman', (request, response) => {
 	response.type('application/json')
 	response.send(JSON.stringify(spiderMan, null, 4))
 })
-
-// Custom 404 page.
-app.use((request, response) => {
-  response.type('text/plain')
-  response.status(404)
-  response.send('404 - Not Found')
-})
-
-// Custom 500 page.
-app.use((err, request, response, next) => {
-  console.error(err.message)
-  response.type('text/plain')
-  response.status(500)
-  response.send('500 - Server Error')
-})
-
-app.listen(port, () => console.log(
-  `Express started at \"http://localhost:${port}\"\n` +
-  `press Ctrl-C to terminate.`)
-)
-*/
-const express			= require('express');
+//const express			= require('express');
 const session			= require('express-session');
 const hbs				= require('express-handlebars');
 const mongoose			= require('mongoose');
 const passport			= require('passport');
 const localStrategy		= require('passport-local').Strategy;
 const bcrypt			= require('bcrypt');
-const app				= express();
+//const app				= express();
 const bodyparser        = require('body-parser');
 var fs = require('fs');
 const User = require(__dirname+'/models/user');
@@ -182,7 +161,7 @@ app.use(passport.session());
 passport.serializeUser(function (user, done) {
 	done(null, user.id);
 });
-const port = process.env.PORT || 3000
+//const port = process.env.PORT || 3000
 passport.deserializeUser(function (id, done) {
 	User.findById(id, function (err, user) {
 		done(err, user);
@@ -305,6 +284,8 @@ app.post('/register/done', async (req, res) => {
 		response.send('500 - Server Error')
 	  })
 	  
-app.listen(port, () => {
-	console.log("Listening on port 3000");
-});
+	  app.listen(port, () => console.log(
+		`Express started at \"http://localhost:${port}\"\n` +
+		`press Ctrl-C to terminate.`)
+	  )
+	  
